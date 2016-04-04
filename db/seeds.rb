c1d7bce5134e4081da67a45e0b24b73eb869265c
mkg1 <gehring.mg@gmail.com>
2016-04-04 09:44:10 -0400

    Add start and end times to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,10 +29,10 @@ location5 = Location.create(address_1: "222 Fayetteville St", address_2: "Suite 
 location6 = Location.create(address_1: "333 Venue Avenue", city: "Durham", state: "NC", zip_code: "27701", nickname: "Wedding venue")
 
 
-post1 = Post.create(title: "Canned goods", details: "300 cans of beans", start_at: "", end_at: "", supplier_id: user1.id, location_id: location1.id)
-post2 = Post.create(title: "Fresh produce", details: "12 lbs of arugula", start_at: "", end_at: "", supplier_id: user2.id, location_id: location2.id)
-post3 = Post.create(title: "Catering leftovers", details: "3 trays of mashed potatoes and 2 trays of brisket", start_at: "", end_at: "", supplier_id: user5.id, location_id: location6.id)
-post4 = Post.create(title: "Misc. grocieries", details: "18 boxes of spaghetti", start_at: "", end_at: "", supplier_id: user1.id, location_id: location1.id)
+post1 = Post.create(title: "Canned goods", details: "300 cans of beans", start_at: "2013-11-22 2:25PM", end_at: "2013-11-29 2:25PM", supplier_id: user1.id, location_id: location1.id)
+post2 = Post.create(title: "Fresh produce", details: "12 lbs of arugula", start_at: "2014-10-12 12:25PM", end_at: "2014-10-13 2:25PM", supplier_id: user2.id, location_id: location2.id)
+post3 = Post.create(title: "Catering leftovers", details: "3 trays of mashed potatoes and 2 trays of brisket", start_at: "2015-3-12 2:25PM", end_at: "2015-3-12 3:25PM", supplier_id: user5.id, location_id: location6.id)
+post4 = Post.create(title: "Misc. grocieries", details: "18 boxes of spaghetti", start_at: "2016-5-12 1:25PM", end_at: "2016-6-12 9:25PM", supplier_id: user1.id, location_id: location1.id)
 
 exchange1 = Exchange.create(claimant_id: user3.id, post_id: post1.id, completed: true)
 exchange2 = Exchange.create(claimant_id: user4.id, post_id: post2.id, completed: true)
