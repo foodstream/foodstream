bb30e6f2fa963ed1673064fbc4985f12e1b5f131
mkg1 <gehring.mg@gmail.com>
2016-04-06 11:40:31 -0400

    Remove first and last name validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :password, presence: true
   validates :email, presence: true, uniqueness: true
   validates_uniqueness_of :token, :allow_blank => true, :allow_nil => true
