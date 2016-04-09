6a5d58e61a47c6d33420b5c548015e1b549726ee
mkg1 <gehring.mg@gmail.com>
2016-04-08 21:23:27 -0400

    Revert back to old app controller temporarily for heroku testing purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  
+
 
   def logged_in?
-    @current_user = (params[:user] && User.find_by(token: params[:user][:token])) || User.find_by(token: params[:token])
-    render json: "Invalid TOKEN" unless @current_user && @current_user.token && @user.id == @current_user.id
+    @current_user = User.find_by(token: params[:token])
+    render json: "Invalid TOKEN" unless params[:token] && @current_user
   end
 
 end
