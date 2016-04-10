d217af862e271bf29632656da143fb30b75a80a8
mkg1 <gehring.mg@gmail.com>
2016-04-10 13:46:00 -0400

    Add method to find average user rating

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,24 +8,19 @@ class User < ActiveRecord::Base
   has_many :locations
   has_many :ratings, class_name: "Rating", foreign_key: "reviewer_id"
   has_many :ratings, class_name: "Rating", foreign_key: "reviewed_id"
+  accepts_nested_attributes_for :ratings, allow_destroy: true
 
   has_secure_password
 
   def generate_token
     update_attribute(:token, SecureRandom.hex) if !token
   end
+
   def destroy_token
     update_attribute(:token, nil)
   end
-  def average_rating
 
-    # reviewedd  = User.find_by(user_id: 4)
-    # average = Average.group(:user).average(:rating)
-    # puts average[4]
-    #
-    #
-    # values = Person.group(:family).maximum(:age) # Person belongs_to :family
-    # puts values[drake]
-    # # => 43
+  def average_rating
+    Rating.where(reviewed_id: id).average(:rating).to_f.round(2)
   end
 end
