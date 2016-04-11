3bc8086361bd0f3e33687d46f2b400ccb9470bf4
mkg1 <gehring.mg@gmail.com>
2016-04-10 22:01:38 -0400

    Change lat long migration to be PG readable

--- db/migrate/20160411001120_change_lat_long_columns.rb
+++ db/migrate/20160411001120_change_lat_long_columns.rb
@@ -1,10 +1,10 @@
 class ChangeLatLongColumns < ActiveRecord::Migration
   def change
     change_column :posts, :latitude, 'decimal USING CAST(latitude AS numeric(12,7))'
-    change_column :posts, :longitude, :decimal, precision: 12, scale: 7
-    change_column :users, :latitude, :decimal, precision: 12, scale: 7
-    change_column :users, :longitude, :decimal, precision: 12, scale: 7
-    change_column :locations, :latitude, :decimal, precision: 12, scale: 7
-    change_column :locations, :longitude, :decimal, precision: 12, scale: 7
+    change_column :posts, :longitude, 'decimal USING CAST(longitude AS numeric(12,7))'
+    change_column :users, :latitude, 'decimal USING CAST(latitude AS numeric(12,7))'
+    change_column :users, :longitude, 'decimal USING CAST(longitude AS numeric(12,7))'
+    change_column :locations, :latitude, 'decimal USING CAST(latitude AS numeric(12,7))'
+    change_column :locations, :longitude, 'decimal USING CAST(longitude AS numeric(12,7))'
   end
 end
