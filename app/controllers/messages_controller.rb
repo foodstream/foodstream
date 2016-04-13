7efea9e5972c41667968dbf5e6386da4ce849882
mkg1 <gehring.mg@gmail.com>
2016-04-13 16:49:21 -0400

    Attempt to render correct json for messages index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @messages = Message.where(post_id: params[:post_id])
-    @messages
+    render json: @messages
   end
 
   def send_sms
