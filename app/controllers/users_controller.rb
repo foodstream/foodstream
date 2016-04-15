3d3741c5d77ea7bc5fa2ac769efac8aec3725c12
mkg1 <gehring.mg@gmail.com>
2016-04-15 16:07:43 -0400

    Add location for update user rendered action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     if @user.update(user_params)
-      render :action => :show
+      render :action => :show, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
